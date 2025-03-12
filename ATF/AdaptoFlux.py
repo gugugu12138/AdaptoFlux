bbca3d01519e0efceae01d99c22268fe6f43c8e0
gugugu12138 <1531483447@qq.com>
2025-03-12 16:29:08 +0800

    分离主程序和示例文件

--- ATF/AdaptoFlux.py
+++ ATF/AdaptoFlux.py
@@ -16,7 +16,7 @@ class CollapseMethod(Enum):
     VARIANCE = 3  # 方差
     PRODUCT = 4   # 相乘
 
-class Figure_Link_Network:
+class AdaptoFlux:
     def __init__(self, values, labels, collapse_method=CollapseMethod.SUM):
         # 存储值(一维元素)
         self.values = values
@@ -456,31 +456,3 @@ class Figure_Link_Network:
         return
         #for i in range(len(self.paths)):
         
-# 示例使用
-if __name__ == "__main__":
-    # 1. 加载 MNIST 数据集
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    # 2. 数据预处理
-    x_train = x_train / 255.0  # 将像素值归一化到 [0, 1]
-    x_test = x_test / 255.0
-
-    # 3. 切割 10% 的数据作为验证集
-    x_train, x_val, y_train, y_val = train_test_split(
-        x_train, y_train, 
-        test_size=0.1,      # 10% 的数据作为验证集
-        random_state=42     # 保证结果的可重复性
-    )
-    print(x_train.reshape(x_train.shape[0], -1).shape)
-    # 3. 载入模型和标签
-    # model = Figure_Link_Network(x_train.reshape(x_train.shape[0], -1),
-    #                             y_train,
-    #                             x_val.reshape(x_val.shape[0], -1),
-    #                             y_val)
-    model = Figure_Link_Network(x_train.reshape(x_train.shape[0], -1),
-                            y_train)
-    
-    
-    file_path = "methods.py"
-    model.import_methods_from_file(file_path)
-    model.training()
--- MLP/ATFMLP.py
+++ MLP/ATFMLP.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.datasets import mnist
+import ATF
 
 # 1. 加载 MNIST 数据集
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 loaded_model = tf.keras.models.load_model('mnist_model.h5')
+x_copy_test, y_copy_test = x_test, y_test
+
 loaded_model.evaluate(x_test, y_test)
+

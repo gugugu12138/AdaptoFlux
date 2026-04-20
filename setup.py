516433ebb7881b0bc82c359bc5a9f42ef0593844
gugugu12138 <1531483447@qq.com>
2026-04-20 14:46:40 +0800

    复原setup

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     description="A dynamic and adaptive function library for dataflow control.",
-    author="",
-    author_email="",
+    author="gugugu12138",
+    author_email="1531483447@qq.com",
     url="https://github.com/gugugu12138/AdaptoFlux",
     install_requires=[
         "tensorflow",
